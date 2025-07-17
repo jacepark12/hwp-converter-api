fe9e13e6fca762618d3557c36274e0a67b60d8ac
pRivAte12 <jspark141515@gmail.com>
2025-07-17 13:29:30 +0900

    :sparkles: Supports hwpx extension

--- logger.py
+++ logger.py
@@ -4,6 +4,8 @@ logger = logging.getLogger("hwp-converter")
 logger.setLevel(logging.DEBUG)
 
 handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+)
 
 logger.addHandler(handler)
--- utils.py
+++ utils.py
@@ -1,5 +1,17 @@
+import os
 import aiofiles.os
 from logger import logger
+
+SUPPORTED_EXTENSIONS = (".hwp", ".hwpx")
+
+
+def is_supported_file(filename: str) -> bool:
+    return filename.lower().endswith(SUPPORTED_EXTENSIONS)
+
+
+def get_file_extension(filename: str) -> str:
+    return os.path.splitext(filename)[1]
+
 
 async def cleanup_file(file_path: str):
     try:
